765cbccb518c7d00ab4fe6e2269ad1edd1eb8d5e
MOMO <seinimohamed01@email.com>
2025-09-12 09:46:07 +0300

    chore(backend): autoriser CORS IP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,10 +18,12 @@ async function bootstrap() {
   // CORS - Permettre Railway et frontend
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'http://localhost:3001',
       'https://backend-de-restaurant-saas-production.up.railway.app',
       /\.railway\.app$/,
-      /\.vercel\.app$/
+      /\.vercel\.app$/,
+      /^http:\/\/(127\.0\.0\.1|192\.168\.[0-9]{1,3}\.[0-9]{1,3}|10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:[0-9]{2,5})?$/,
     ],
     credentials: true,
   });
@@ -29,7 +31,7 @@ async function bootstrap() {
   // Prefix API global
   app.setGlobalPrefix('api/v1');
   
-  const port = configService.get('PORT') || 3000;
+  const port = configService.get('PORT') || 3001;
   await app.listen(port);
   
   console.log(`🚀 Application démarrée sur le port ${port}`);
